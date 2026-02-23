f05e32b0597d849a18402d5e0f46c4a6b2db5138
Gabriel Passos DEV <gabrielpassos.ti@gmail.com>
2026-02-23 20:04:18 -0300

    styling base ui

--- frontend/src/components/FinancialList.jsx
+++ frontend/src/components/FinancialList.jsx
@@ -3,5 +3,5 @@ export default function FinancialList() {
     <div className="bg-slate-800 p-8 rounded-xl shadow-md w-80 flex flex-col gap-4 justify-center items-center">
         
     </div>
-  );
+  );  
 }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import ViwHistoryFinancial from "../components/ViwHistoryFinancial";
 
 export default function Dashboard() {
   return (
-    <div className="bg-gray-400 min-h-screen w-full flex flex-col gap-4 items-center justify-center">
+   <div className="bg-gray-400 min-h-screen w-full flex flex-col gap-4 justify-center items-center px-4 overflow-x-hidden">
       <CreateFinancial />
       <ViwHistoryFinancial />
     </div>
