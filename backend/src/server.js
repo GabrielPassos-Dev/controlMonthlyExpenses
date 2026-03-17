d3081db91ae46dec272745010794d9ffd4d21c3c
Gabriel Passos DEV <gabrielpassos.ti@gmail.com>
2026-03-16 23:30:57 -0300

    update start and port

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 import app from "./app.js";
 
-app.listen(3000, () => {
-  console.log("Server running");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
